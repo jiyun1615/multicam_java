ccff2d393919059630ab62904f834e0acd5dd95e
Jay <51588209+jiyun1615@users.noreply.github.com>
2023-02-02 17:40:14 +0900

    day04 upload

---
+++ day04/out.js
@@ -0,0 +1,22 @@
+// out.js
+
+function hello(){
+    document.write("1.Hello ");
+    document.write("2.Hello ");
+    document.write("3.Hello ");
+    document.write("4.Hello ");
+}
+
+function cleanHouse(){
+    document.write("1.청소해 ");
+    document.write("2. 아 졸려 ");
+}
+
+function travel(){
+    document.write("여행가자")
+}
+
+function rest(){
+    document.write("집에서 쉬자")
+}
+
