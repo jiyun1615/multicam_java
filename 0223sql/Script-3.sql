5d56007a9d501b09fc577bc974f6aa7785bcbbc5
Jay <51588209+jiyun1615@users.noreply.github.com>
2023-02-23 17:55:52 +0900

    oracle 다운로드 및 sql 기초

---
+++ 0223sql/Script-3.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "HR"."MEMBER3" 
+   (	"ID" VARCHAR2(100), 
+	"PW" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"TEL" VARCHAR2(100)
+   )
+   
+   
+   
+   INSERT INTO "member" VALUES ('Park', '1234','yunn', '011')
+   
+   INSERT INTO "member" VALUES ('apple', '1234', 'apple', '010')
+   
+   INSERT INTO "member" VALUES ('ice', '1111','ice', '011')
+   
+   INSERT INTO "member" VALUES ('melon', '3333','melon', '012')
+  
+   
+   
+   
+ SELECT * FROM "member"
+   
